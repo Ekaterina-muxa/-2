b77097a44723dcd4d041e85edcd5f9a7b68a34da
«Ekaterina» <katuxa-muxa@mail.ru>
2022-12-05 20:38:13 +0200

    решение задач

---
+++ hw2/Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+17 -> такого числа в массиве нет
+*/
+
+
+int[] CreateArray (int size)
+{
+    int [] newArray = new int [size];
+    for(int i=0; i < size; i++)
+    {
+      Console.WriteLine ($"введите элемент {i}");
+      newArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return newArray;
+}
+
+void ShowArray (int[] array)
+{
+    Console.Write ("получившийся массив - >");
+    for (int i=0; i< array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+
+}
+
+void number1(int[] array)
+{
+    Console.WriteLine ($"введите номер элемента");
+    int number = Convert.ToInt32(Console.ReadLine());
+    for (int i = 0; i< array.Length; i++)
+    {
+        if (number == (i)) 
+        {
+            Console.WriteLine ($"элемент под данным номером равен - > ", array[i]);
+        }
+        else 
+        {
+            Console.WriteLine ($" такого элемента нет"); 
+        }
+
+    }
+    
+}
+
+
+Console.Write($"введите размер массива");
+int a = Convert.ToInt32(Console.ReadLine());
+int [] myArray = CreateArray (a);
+ShowArray(myArray);
+number1(myArray);
---
+++ hw3/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+
+Console.WriteLine("Задайте количество строк двумерного массива:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте количество столбцов двумерного массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void PrintArray(int[,] matr)
+{ 
+ for (int i = 0; i < m; i++)
+ { 
+    for (int j = 0; j < n; j++)
+ { 
+    Console.Write($"{matr[i, j]} ");
+ }
+ Console.WriteLine();
+ }
+ }
+
+void FillArray(int[,] matr)
+{ 
+    for (int i = 0; i < m; i++)
+ { 
+    for (int j = 0; j < n; j++)
+ { 
+    matr[i,j] = new Random().Next(1,100);}
+ }
+}
+
+void AverageArray(int[,] matr)
+{
+for (int j = 0; j < n; j++)
+{
+    int sum = 0;
+    for (int i = 0; i < m; i++)
+    {
+        sum = sum + matr[i, j];
+    }
+    Console.WriteLine($"{ sum / m} ");
+}
+}
+
+int[,] matrix = new int[m,n];
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+AverageArray(matrix);
